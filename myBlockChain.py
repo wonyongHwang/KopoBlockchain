6420f66bdc65a09cf99366e7b4492d7c06b06d42
김동휘 <kimdh4033@naver.com>
2020-05-19 19:13:22 +0900

    Revert "Test 4 추가"
    This reverts commit 96be6deef87f80f50e048d2e3935e2bfd7230259.

--- myBlockChain.py
+++ myBlockChain.py
@@ -20,7 +20,7 @@ from multiprocessing import Process, Lock # for using Lock method(acquire(), rel
 # test
 # test2
 # Test 3 
-# Test 4
+
 # for Put Lock objects into variables(lock)
 lock = Lock()
 
