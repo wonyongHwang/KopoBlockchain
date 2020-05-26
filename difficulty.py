ce29e3680e813ef87ed3463258205c70acfe8a78
Kim Jae-Young <miracle013@naver.com>
2020-05-26 17:19:38 +0900

    diff complete
    end

--- difficulty.py
+++ difficulty.py
@@ -1,46 +1,56 @@
-###difficulty ver.0
-###1. 디버깅 필요
-###2. 해당함수 투입지점 필요(아마도 mine에 넣으면 될것으로 예상)
+import csv
+g_cycle = 10
+g_difficulty = 2
+g_bcFileName = "blockchain.csv"
+importedBlockchain = []
 
-### m_cycle변수는 난이도 갱신주기
-###calculateDifficulty 함수는 출력 시 m_difficulty를 변수로 받아서
-###번역변수의 초기화 필요
-###
-
-
+with open(g_bcFileName, 'r',  newline='') as file:
+    blockReader = csv.reader(file)
+    for line in blockReader:
+        importedBlockchain.append([line[0], line[1], line[2], line[3], line[4], line[5]])
 
 def countZero(previousdiff):
     for i in range(1,64):
-    checkzero = hashsample[0:i].count("0")
-    print(checkzero)
-    if (checkzero != i):
-        return i-1
+        checkzero = previousdiff[0:i].count("0")
+        print(checkzero)
+        if (checkzero != i):
+            return i-1
 
 
-def calculateDifficulty(blockchainPath = g_bcFileName, difficulty = g_difficulty, visitValue = g_cycle):
+def calculateDifficulty(blockchainPath = importedBlockchain, difficulty = g_difficulty, visitValue = g_cycle):
 
-    blockchain = readBlockchain(blockchainPath)
+    blockchain = blockchainPath
     sumTime = 0
 
-    previousdiff= blockchain[-1].currentHash
-    if(int(blockchain[-1]['index'])<visitValue):
+    previousdiff= blockchain[-1][4]
+    print(previousdiff)
+
+    #제네시스 블록생성시 예외
+    if(int(blockchain[-1][0])<visitValue):
         return difficulty
     else:
 
-        if (int(blockchain[-1]['index'])%visitValue == 0):
-            endblockIndex = int(blockchain[-1]['index'])
+        if (int(blockchain[-1][0])%visitValue == 0):
+            endblockIndex = int(blockchain[-1][0])
             startblockIndex = endblockIndex - visitValue
-            for i in range(startblockIndex-1, endblockIndex):
-                sumTime += blockchain[i]['timestamp']
-
+            timegap = int(blockchain[endblockIndex][2]) - int(blockchain[startblockIndex][2])
+#             for i in range(startblockIndex-1, endblockIndex):
+#                 #수정필요, 합이 아닌 마지막 스탬프와 처음스탬프의 차만 있으면됨
+#                 sumTime += int(blockchain[i][2])
             problemDiff = countZero(previousdiff)
-            if(sumTime > visitValue*700):
+            print(timegap)
+            print(visitValue)
+            if(timegap > visitValue*700):
                 difficulty = problemDiff -1
                 return difficulty
-            elif(sumTime >= visitValue*500):
+            elif(timegap >= visitValue*500):
                 return difficulty
             else:
                 difficulty = problemDiff + 1
                 return difficulty
         else:
             return difficulty
+
+
+
+calculateDifficulty()
